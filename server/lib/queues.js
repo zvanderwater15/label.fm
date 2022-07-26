be7a01f0f772f7546f4e855f0995bf9c9feab3a0
zvanderwater15 <zvanderwater15@georgefox.edu>
2022-07-25 21:38:57 -0700

    send and receive queue message functions

---
+++ server/lib/queues.js
@@ -0,0 +1,34 @@
+import amqp from "amqplib";
+
+export async function sendMessage(msg) {
+  amqp.connect(process.env.AMQP_URL).then((connection) =>
+    connection.createChannel().then((channel) => {
+      channel.assertQueue(process.env.QUEUE_NAME, {
+        durable: false,
+      });
+      channel.sendToQueue(queue, Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+    })
+  );
+  setTimeout(function () {
+    connection.close();
+  }, 500);
+}
+
+export async function receiveMessage(callback) {
+  return amqp.connect(process.env.AMQP_URL).then((connection) =>
+    connection.createChannel().then((channel) => {
+      channel.assertQueue(process.env.QUEUE_NAME, {
+        durable: false,
+      });
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        process.env.QUEUE_NAME
+      );
+
+      channel.consume(process.env.QUEUE_NAME, callback, {
+        noAck: false,
+      });
+    })
+  );
+}
