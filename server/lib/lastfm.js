15a6e7044e11243ab22cb5ead8287166f2c34405
zvanderwater15 <zvanderwater15@georgefox.edu>
2022-08-05 12:54:03 -0700

    add api call to get last.fm friends

--- server/lib/lastfm.js
+++ server/lib/lastfm.js
@@ -18,3 +18,14 @@ export async function getTopTracks(username, limit=100) {
       throw new Error("User not found")
     }
   }
+
+  export async function getFriends(username, limit=500) {
+    const url = `http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user=${username}&limit=${limit}&api_key=${process.env.API_KEY}&format=json`
+    const res = await fetch(url)
+    const userJson = await res.json()
+    if (userJson.friends) {
+      return {users: userJson.friends.user}
+    } else {
+      return {users: []}
+    }
+  }
